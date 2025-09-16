d1ac5587aa3be1415d49ae306efddacd322ca549
GingerGigiCat <cat@gigicat.co.uk>
2025-09-16 22:09:24 +0100

    made a very basic homework card

--- app/src/main/java/com/gigi/classchartsandroid/MainActivity.kt
+++ app/src/main/java/com/gigi/classchartsandroid/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.gigi.classchartsandroid
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gigi.classchartsandroid.ui.theme.ClassChartsAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +29,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ClassChartsAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeworkCard(
+                        homework = Homework("Music", "this is a music homework you have to do a lot of work for this because obviously of course you do what more would you expect from homework and this is supposed to be a really loioooonmg description explaining everything you need to do for the task like questyion a question b question cquestion d and all of thsose so that it can show what happens when the content is long, hopefully it will collapse the text and then you can see the whole thing when you clickk on me but who knows"),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +39,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Homework(val title: String, val body: String) //val startDate: String, val endDate: String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeworkCard(homework: Homework, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.fillMaxWidth().padding(10.dp)) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = homework.title, style = typography.titleLarge)
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = homework.body)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     ClassChartsAndroidTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            HomeworkCard(
+                homework = Homework("Music", "this is a music homework you have to do a lot of work for this because obviously of course you do what more would you expect from homework and this is supposed to be a really loioooonmg description explaining everything you need to do for the task like questyion a question b question cquestion d and all of thsose so that it can show what happens when the content is long, hopefully it will collapse the text and then you can see the whole thing when you clickk on me but who knows"),
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
