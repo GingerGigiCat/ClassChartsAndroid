d88bddffd312b49290e3f34730ac64abc6f8eb4f
GingerGigiCat <cat@gigicat.co.uk>
2025-09-20 00:07:42 +0100

    added show only incomplete homework toggle to the ui

--- app/src/main/java/com/gigi/classchartsandroid/MainActivity.kt
+++ app/src/main/java/com/gigi/classchartsandroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.gigi.classchartsandroid
 
+import android.R
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +20,7 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +29,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gigi.classchartsandroid.ui.theme.ClassChartsAndroidTheme
+import java.util.Date
 import kotlin.math.truncate
 
 class MainActivity : ComponentActivity() {
@@ -48,7 +52,7 @@ class MainActivity : ComponentActivity() {
 data class Homework(val title: String, val complete: Boolean, val teacher: String, val subject: String, val body: String) //val startDate: String, val endDate: String)
 
 @Composable
-fun HomeworkCard(homework: Homework, modifier: Modifier = Modifier) {
+fun HomeworkCard(homework: Homework, modifier: Modifier = Modifier, compact: Boolean = false) {
     Card(modifier = modifier.fillMaxWidth().padding(10.dp)) {
         Column(modifier = Modifier.padding(15.dp)) {
             Row(
@@ -56,12 +60,44 @@ fun HomeworkCard(homework: Homework, modifier: Modifier = Modifier) {
             ){
                 Checkbox(checked = false, onCheckedChange = null)
                 Spacer(modifier = Modifier.width(15.dp))
-                Text(text = homework.title, style = typography.titleLarge, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                Column {
+                    Text(
+                        text = homework.title,
+                        style = typography.titleLarge,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    if (compact) {
+                        Text(
+                            text = homework.subject + " - " + homework.teacher,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
             }
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(text = homework.subject + " - " + homework.teacher, maxLines = 1, overflow = TextOverflow.Ellipsis)
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(text = homework.body, maxLines = 3, overflow = TextOverflow.Ellipsis)
+            if (!compact) {
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = homework.subject + " - " + homework.teacher,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(text = homework.body, maxLines = 3, overflow = TextOverflow.Ellipsis)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ShowCompletedHomeworksToggle() {
+    ClassChartsAndroidTheme {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(15.dp)) {
+            Text(text = "Only show incomplete homework")
+            Spacer(modifier = Modifier.weight(1f))
+            Switch(true, onCheckedChange = null)
         }
     }
 }
@@ -71,10 +107,29 @@ fun HomeworkCard(homework: Homework, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     ClassChartsAndroidTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            HomeworkCard(
-                homework = Homework(title = "Modal Jazz Improvisation", complete = false, teacher = "Mr. Teacher", subject = "Music", body = "this is a music homework you have to do a lot of work for this because obviously of course you do what more would you expect from homework and this is supposed to be a really loioooonmg description explaining everything you need to do for the task like questyion a question b question cquestion d and all of thsose so that it can show what happens when the content is long, hopefully it will collapse the text and then you can see the whole thing when you clickk on me but who knows"),
-                modifier = Modifier.padding(innerPadding)
-            )
+            Column(modifier = Modifier.padding(innerPadding)) {
+                ShowCompletedHomeworksToggle()
+                HomeworkCard(
+                    homework = Homework(
+                        title = "Modal Jazz Improvisation",
+                        complete = false,
+                        teacher = "Mr. Teacher",
+                        subject = "Music",
+                        body = "this is a music homework you have to do a lot of work for this because obviously of course you do what more would you expect from homework and this is supposed to be a really loioooonmg description explaining everything you need to do for the task like questyion a question b question cquestion d and all of thsose so that it can show what happens when the content is long, hopefully it will collapse the text and then you can see the whole thing when you clickk on me but who knows"
+                    ),
+                    compact = true
+                )
+                HomeworkCard(
+                    homework = Homework(
+                        title = "Modal Jazz Improvisation",
+                        complete = false,
+                        teacher = "Mr. Teacher",
+                        subject = "Music",
+                        body = "this is a music homework you have to do a lot of work for this because obviously of course you do what more would you expect from homework and this is supposed to be a really loioooonmg description explaining everything you need to do for the task like questyion a question b question cquestion d and all of thsose so that it can show what happens when the content is long, hopefully it will collapse the text and then you can see the whole thing when you clickk on me but who knows"
+                    ),
+                    compact = false
+                )
+            }
         }
     }
 }
