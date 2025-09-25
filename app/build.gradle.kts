f2958f0bca8201c7573d6f3c24f368b857146e04
GingerGigiCat <gingerisacat@outlook.com>
2025-09-25 21:55:26 +0100

    attempting to do web requests jsonness, might have to use Jackson instead of Moshi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.squareup.okhttp3:okhttp:5.1.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
 }
--- app/src/main/java/com/gigi/classchartsandroid/RequestMaker.kt
+++ app/src/main/java/com/gigi/classchartsandroid/RequestMaker.kt
@@ -1,14 +1,39 @@
 package com.gigi.classchartsandroid
-import java.net.URI
-import android.net.http.HttpClient
-import android.net.http.HttpRequest
-import android.net.http.HttpResponse
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.FormBody
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class RequestMaker {
-
+    private val client = OkHttpClient()
     var token: String? = null
     constructor()
 
     constructor(id: String, dob: String) {
+        val requestBody = FormBody.Builder()
+            .add("code", id)
+            .add("remember", "true")
+            .add("recaptcha-token", "no-token-available")
+            .add("dob", dob)
+            .build()
+
+        val request = Request.Builder()
+            .url("https://www.classcharts.com/apiv2student/login")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw _root_ide_package_.okio.IOException("Unexpected code $response")
+
+            val jsonResponse = response.body.string()
+            val moshi: Moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+            val jsonAdapter: JsonAdapter = moshi.adapter(User::class.java)
+        }
     }
+
 }
