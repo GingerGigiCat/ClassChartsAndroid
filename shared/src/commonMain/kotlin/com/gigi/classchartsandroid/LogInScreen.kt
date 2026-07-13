2fdf68faafc67d1b9fe6cf7c0d599c706d926cd1
GingerGigiCat <cat@gigicat.co.uk>
2026-07-13 18:07:23 +0100

    fixed LogInScreen.kt imports

--- shared/src/commonMain/kotlin/com/gigi/classchartsandroid/LogInScreen.kt
+++ shared/src/commonMain/kotlin/com/gigi/classchartsandroid/LogInScreen.kt
@@ -1,19 +1,41 @@
 package com.gigi.classchartsandroid
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.touchlab.kermit.Logger
 import com.gigi.cca.shared.ErrorInvalidLogin
 import com.gigi.cca.shared.ErrorType
 import com.gigi.cca.shared.RequestMaker
 import com.gigi.cca.shared.ui.DatePickerButton
 import com.gigi.cca.shared.ui.DoDatePicker
 import com.gigi.cca.shared.ui.theme.ClassChartsAndroidTheme
+import kotlinx.coroutines.runBlocking
+import java.time.LocalDate
 
 @Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
@@ -74,7 +96,7 @@ fun LogInScreen(modifier:Modifier = Modifier, requestMaker: RequestMaker, naviga
                             dob = getLocalDateObjectForSelected().toString()
                         )
                     }
-                    Log.d("RealLoginResponse", loginResponse.toString())
+                    Logger.d("RealLoginResponse") {loginResponse.toString()}
                     navigate()
                     doReturn = false
                     doReturn = true
