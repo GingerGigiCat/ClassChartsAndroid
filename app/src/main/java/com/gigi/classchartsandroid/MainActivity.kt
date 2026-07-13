238f0b20772bf11f3c838910ae529a2f9b759b40
GingerGigiCat <cat@gigicat.co.uk>
2026-07-13 18:05:22 +0100

    moved LogInScreen.kt, now fixing imports

--- app/src/main/java/com/gigi/classchartsandroid/MainActivity.kt
+++ app/src/main/java/com/gigi/classchartsandroid/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.os.StrictMode
 import android.util.Log
-import android.webkit.MimeTypeMap
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,28 +14,16 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.DisplayMode
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -55,7 +42,6 @@ import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.fromHtml
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -64,10 +50,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.gigi.cca.shared.ui.DateDivider
-import com.gigi.cca.shared.ui.DatePickerButton
-import com.gigi.cca.shared.ui.DoDatePicker
 import com.gigi.cca.shared.ErrorInvalidLogin
-import com.gigi.cca.shared.ErrorType
 import com.gigi.cca.shared.Homework
 import com.gigi.cca.shared.ui.HomeworkCard
 import com.gigi.cca.shared.ui.HomeworkContent
@@ -358,85 +341,6 @@ fun HomeworkList(requestMaker: RequestMaker, homeworksList: MutableList<Homework
     }
 }
 """
-
-
-@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun LogInScreenPreview() {
-    ClassChartsAndroidTheme {
-        Scaffold(Modifier.fillMaxSize()) { innerPadding ->
-            LogInScreen(Modifier.padding(innerPadding), RequestMaker())
-        }
-    }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun LogInScreen(modifier:Modifier = Modifier, requestMaker: RequestMaker, navigate: () -> Unit = {}): ErrorType {
-    var dateState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
-    var showDatePicker by remember { mutableStateOf(false) }
-    var studentId by remember { mutableStateOf(requestMaker.studentId?: "") }
-    val getLocalDateObjectForSelected = { LocalDate.ofEpochDay(dateState.selectedDateMillis!!.toLong() / (24 * 60 * 60 * 1000)) }
-    var goToHomeworkList by remember { mutableStateOf(false) }
-    var loginResponse: ErrorType = ErrorInvalidLogin()
-    var doReturn by remember { mutableStateOf(true) }
-    if (dateState.selectedDateMillis == null) dateState.selectedDateMillis = 1230768000000
-
-    Column(modifier.padding(10.dp).verticalScroll(rememberScrollState())) {
-        Spacer(Modifier.height(30.dp))
-        Text(
-            "Login",
-            style = MaterialTheme.typography.displayLarge,
-            color = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.padding(15.dp)
-        )
-        Text(
-            "Login with classcharts code and DoB",
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(start = 18.dp)
-        )
-        Spacer(Modifier.height(10.dp))
-        OutlinedTextField(
-            studentId,
-            onValueChange = {studentId = it},
-            Modifier.padding(start = 15.dp, end = 15.dp).fillMaxWidth(),
-            label = { Text("Classcharts code") })
-        Spacer(Modifier.height(10.dp))
-        DatePickerButton(getLocalDateObjectForSelected, { showDatePicker = true })
-        if (showDatePicker) DoDatePicker(dateState, { theState ->
-            dateState = theState
-            showDatePicker = false
-        })
-        Spacer(Modifier.height(15.dp))
-        Row {
-            Spacer(Modifier.weight(1f))
-            Button(
-                onClick = {
-                    runBlocking {
-                        loginResponse = requestMaker.login(
-                            id = studentId,
-                            dob = getLocalDateObjectForSelected().toString()
-                        )
-                    }
-                    Log.d("RealLoginResponse", loginResponse.toString())
-                    navigate()
-                    doReturn = false
-                    doReturn = true
-                }, content = { Text("Login") }, modifier = Modifier
-                .padding(end = 15.dp), colors = ButtonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary,
-                    disabledContainerColor = MaterialTheme.colorScheme.inversePrimary,
-                    disabledContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            )
-        }
-    }
-    if (doReturn) return loginResponse
-    else return loginResponse
-}
-
 
 
 @Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
---
+++ shared/src/commonMain/kotlin/com/gigi/classchartsandroid/LogInScreen.kt
@@ -0,0 +1,93 @@
+package com.gigi.classchartsandroid
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+import com.gigi.cca.shared.ErrorInvalidLogin
+import com.gigi.cca.shared.ErrorType
+import com.gigi.cca.shared.RequestMaker
+import com.gigi.cca.shared.ui.DatePickerButton
+import com.gigi.cca.shared.ui.DoDatePicker
+import com.gigi.cca.shared.ui.theme.ClassChartsAndroidTheme
+
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun LogInScreenPreview() {
+    ClassChartsAndroidTheme {
+        Scaffold(Modifier.fillMaxSize()) { innerPadding ->
+            LogInScreen(Modifier.padding(innerPadding), RequestMaker())
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LogInScreen(modifier:Modifier = Modifier, requestMaker: RequestMaker, navigate: () -> Unit = {}): ErrorType {
+    var dateState = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+    var showDatePicker by remember { mutableStateOf(false) }
+    var studentId by remember { mutableStateOf(requestMaker.studentId?: "") }
+    val getLocalDateObjectForSelected = { LocalDate.ofEpochDay(dateState.selectedDateMillis!!.toLong() / (24 * 60 * 60 * 1000)) }
+    var goToHomeworkList by remember { mutableStateOf(false) }
+    var loginResponse: ErrorType = ErrorInvalidLogin()
+    var doReturn by remember { mutableStateOf(true) }
+    if (dateState.selectedDateMillis == null) dateState.selectedDateMillis = 1230768000000
+
+    Column(modifier.padding(10.dp).verticalScroll(rememberScrollState())) {
+        Spacer(Modifier.height(30.dp))
+        Text(
+            "Login",
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(15.dp)
+        )
+        Text(
+            "Login with classcharts code and DoB",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(start = 18.dp)
+        )
+        Spacer(Modifier.height(10.dp))
+        OutlinedTextField(
+            studentId,
+            onValueChange = {studentId = it},
+            Modifier.padding(start = 15.dp, end = 15.dp).fillMaxWidth(),
+            label = { Text("Classcharts code") })
+        Spacer(Modifier.height(10.dp))
+        DatePickerButton(getLocalDateObjectForSelected, { showDatePicker = true })
+        if (showDatePicker) DoDatePicker(dateState, { theState ->
+            dateState = theState
+            showDatePicker = false
+        })
+        Spacer(Modifier.height(15.dp))
+        Row {
+            Spacer(Modifier.weight(1f))
+            Button(
+                onClick = {
+                    runBlocking {
+                        loginResponse = requestMaker.login(
+                            id = studentId,
+                            dob = getLocalDateObjectForSelected().toString()
+                        )
+                    }
+                    Log.d("RealLoginResponse", loginResponse.toString())
+                    navigate()
+                    doReturn = false
+                    doReturn = true
+                }, content = { Text("Login") }, modifier = Modifier
+                .padding(end = 15.dp), colors = ButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                    disabledContainerColor = MaterialTheme.colorScheme.inversePrimary,
+                    disabledContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        }
+    }
+    if (doReturn) return loginResponse
+    else return loginResponse
+}
